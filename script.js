4321dc2b9a01ff547289663d7ff977371d40a074
Dara <adara@hagmanfamily.com>
2023-08-23 02:20:26 -0400

    Add files via upload
    1. Separated recommendations to show up individually
    2. Added buttons to move back and forth between recommendations

--- script.js
+++ script.js
@@ -1,16 +1,74 @@
+//PET option pages 
 
-function PET() {
-  window.location.href = "PET.html";
+function PETOne() {
+  window.location.href = "petone.html";
 }
 
-function PE() {
-  window.location.href = "PE.html";
+function PETTwo() {
+  window.location.href = "pettwo.html";
 }
 
-function PP() {
-  window.location.href = "PP.html";
+function PETThree() {
+  window.location.href = "petthree.html"
 }
 
-function PLA() {
-  window.location.href = "PLA.html";
+function PETFour() {
+  window.location.href = "petfour.html"
+}
+
+function PETFive() {
+  window.location.href = "petfive.html"
+}
+
+function PETSix() {
+  window.location.href = "petsix.html"
+}
+
+// PE option pages 
+
+function PEOne() {
+  window.location.href = "peone.html";
+}
+
+function PETwo() {
+  window.location.href = "petwo.html";
+}
+
+function PEThree() {
+  window.location.href = "pethree.html";
+}
+
+function PEFour() {
+  window.location.href = "pefour.html";
+}
+
+// PP option pages
+
+function PPOne() {
+  window.location.href = "ppone.html";
+}
+
+function PPTwo() {
+  window.location.href = "pptwo.html";
+}
+
+function PPThree() {
+  window.location.href = "ppthree.html";
+}
+
+function PPFour() {
+  window.location.href = "ppfour.html";
+}
+
+function PPFive() {
+  window.location.href = "ppfive.html";
+}
+
+// PLA option pages 
+function PLAOne() {
+  window.location.href = "plaone.html";
+}
+
+function PLATwo() {
+  window.location.href = "platwo.html";
 }
