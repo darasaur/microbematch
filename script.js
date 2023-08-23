fc953150c25c855e3035dad9ec76c7b486ce33a9
Dara <adara@hagmanfamily.com>
2023-08-23 00:43:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function PET() {
+  window.location.href = "PET.html";
+}
+
+function PE() {
+  window.location.href = "PE.html";
+}
+
+function PP() {
+  window.location.href = "PP.html";
+}
+
+function PLA() {
+  window.location.href = "PLA.html";
+}
